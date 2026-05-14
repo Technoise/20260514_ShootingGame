0f655c00c02f45f14f63dd522da3275b66b9bcd2
Technoise <technoise.kor@gmail.com>
2026-05-14 17:31:47 +0900

    최고점수 추가

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -4,28 +4,40 @@ using UnityEngine.UI;
 public class ScoreManager : MonoBehaviour
 {
     [SerializeField] private Text currentScoreText;
+    [SerializeField] private Text bestScoreText;
 
     private int currentScore;
+    private int bestScore;
 
     private void Start()
     {
         currentScore = 0;
+        bestScore = 0;
         UpdateScoreText();
     }
 
     public void AddScore()
     {
         currentScore += 1;
+
+        if (currentScore > bestScore)
+        {
+            bestScore = currentScore;
+        }
+
         UpdateScoreText();
     }
 
     private void UpdateScoreText()
     {
-        if (currentScoreText == null)
+        if (currentScoreText != null)
         {
-            return;
+            currentScoreText.text = "현재 점수 : " + currentScore;
         }
 
-        currentScoreText.text = "현재 점수 : " + currentScore;
+        if (bestScoreText != null)
+        {
+            bestScoreText.text = "최고 점수 : " + bestScore;
+        }
     }
 }
