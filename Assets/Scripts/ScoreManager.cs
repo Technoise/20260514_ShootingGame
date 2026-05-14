7bb09cb4e2922a70860039a99738d5cf21df54ec
Technoise <technoise.kor@gmail.com>
2026-05-14 17:41:27 +0900

    최고 점수 저장 및 로드 추가

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -3,6 +3,8 @@ using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
+    private const string BestScoreKey = "BestScore";
+
     [SerializeField] private Text currentScoreText;
     [SerializeField] private Text bestScoreText;
 
@@ -12,7 +14,7 @@ public class ScoreManager : MonoBehaviour
     private void Start()
     {
         currentScore = 0;
-        bestScore = 0;
+        bestScore = PlayerPrefs.GetInt(BestScoreKey, 0);
         UpdateScoreText();
     }
 
@@ -23,9 +25,16 @@ public class ScoreManager : MonoBehaviour
         if (currentScore > bestScore)
         {
             bestScore = currentScore;
+            SaveBestScore();
         }
 
         UpdateScoreText();
+    }
+
+    private void SaveBestScore()
+    {
+        PlayerPrefs.SetInt(BestScoreKey, bestScore);
+        PlayerPrefs.Save();
     }
 
     private void UpdateScoreText()
