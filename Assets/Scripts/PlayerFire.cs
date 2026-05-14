5b2137baf0502d1c23f07117f59d8d5e63f59537
Technoise <technoise.kor@gmail.com>
2026-05-14 17:55:27 +0900

    자동발사 추가

--- Assets/Scripts/PlayerFire.cs
+++ Assets/Scripts/PlayerFire.cs
@@ -4,12 +4,31 @@ public class PlayerFire : MonoBehaviour
 {
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private Transform firePoint;
+    [SerializeField] private float autoFireInterval = 0.2f;
+
+    private float autoFireTimer;
 
     private void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
             FireBullet();
+            autoFireTimer = 0f;
+        }
+
+        if (Input.GetButton("Fire1"))
+        {
+            autoFireTimer += Time.deltaTime;
+
+            if (autoFireTimer >= autoFireInterval)
+            {
+                FireBullet();
+                autoFireTimer = 0f;
+            }
+        }
+        else
+        {
+            autoFireTimer = 0f;
         }
     }
 
